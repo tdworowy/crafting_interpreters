d0423640c51d354023d4baf5c9c34fc42ae83fa2
Tomasz Dworowy <dworowytomasz@gmail.com>
2025-05-08 16:31:31 +0200

    fix unit tests

--- interpreter_in_python/tests/parser_tests.py
+++ interpreter_in_python/tests/parser_tests.py
@@ -1,29 +1,34 @@
 from src.expr import Binary, Literal
 from src.parser import Parser
+from src.stmt import Expression
 from src.token_ import Token, TokenType
 
 
 def test_parser_correct():
     minus_token = Token(TokenType.MINUS, lexeme="-", literal=None, line=0)
-    expr = Parser(
+    statements = Parser(
         tokens=[
             Token(TokenType.NUMBER, lexeme="1", literal=1, line=1),
             minus_token,
             Token(TokenType.NUMBER, lexeme="5", literal=5, line=2),
-            Token(TokenType.EOF, lexeme="", literal=None, line=3),
+            Token(TokenType.SEMICOLON, lexeme=";", literal=None, line=3),
+            Token(TokenType.EOF, lexeme="", literal=None, line=4),
         ]
     ).parse()
-    assert expr == Binary(
-        left=Literal(value=1), operator=minus_token, right=Literal(value=5)
+    assert statements[0] == Expression(
+        expression=Binary(
+            left=Literal(value=1), operator=minus_token, right=Literal(value=5)
+        )
     )
 
 
 def test_parser_binary_without_left():
-    expr = Parser(
+    statements = Parser(
         tokens=[
             Token(TokenType.PLUS, lexeme="+", literal=None, line=0),
             Token(TokenType.NUMBER, lexeme="5", literal=5, line=1),
-            Token(TokenType.EOF, lexeme="", literal=None, line=2),
+            Token(TokenType.SEMICOLON, lexeme=";", literal=None, line=3),
+            Token(TokenType.EOF, lexeme="", literal=None, line=4),
         ]
     ).parse()
-    assert expr is None
+    assert statements[0] is None
