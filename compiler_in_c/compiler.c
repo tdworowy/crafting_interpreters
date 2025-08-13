348baeea4a1fd1b0bec035777eecf6004bc5d8f6
Tomasz Dworowy <dworowytomasz@gmail.com>
2025-08-13 15:19:09 +0200

    update

--- compiler_in_c/compiler.c
+++ compiler_in_c/compiler.c
@@ -484,13 +484,49 @@ static void emitLoop(const int loopStart) {
 }
 
 static void forStatement() {
-  consume(TOKEN_LEFT_PAREN, "Expect ')' after 'for'.");
-  consume(TOKEN_SEMICOLON, "Expect ';'.");
+  beginScope();
+
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+  if (match(TOKEN_SEMICOLON)) {
+
+  } else if (match(TOKEN_VAR)) {
+    varDeclaration();
+  } else {
+    expressionStatement();
+  }
+
   int loopStart = currentChunk()->count;
-  consume(TOKEN_SEMICOLON, "Expect ';'.");
-  consume(TOKEN_RIGHT_PAREN, "Expect ')' after clauses.");
+  int exitJump = -1;
+  if (!match(TOKEN_SEMICOLON)) {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';'.");
+
+    exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+  }
+
+  if (!match(TOKEN_RIGHT_PAREN)) {
+    const int bodyJump = emitJump(OP_JUMP);
+    const int incrementStart = currentChunk()->count;
+
+    expression();
+    emitByte(OP_POP);
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+    emitLoop(loopStart);
+    loopStart = incrementStart;
+    patchJump(bodyJump);
+  }
+
   statement();
   emitLoop(loopStart);
+
+  if (exitJump != -1) {
+    patchJump(exitJump);
+    emitByte(OP_POP);
+  }
+
+  endScope();
 }
 
 static void ifStatement() {
