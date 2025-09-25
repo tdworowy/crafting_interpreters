6280be599bcb6b15195a09da3ce73edf9118e5ab
Tomasz Dworowy <dworowytomasz@gmail.com>
2025-09-25 16:36:10 +0200

    update

--- compiler_in_c/chunk.h
+++ compiler_in_c/chunk.h
@@ -40,6 +40,9 @@ typedef enum {
   OP_GET_PROPERTY,
   OP_METHOD,
   OP_INVOKE,
+  OP_INHERIT,
+  OP_GET_SUPER,
+  OP_SUPER_INVOKE
 } OpCode;
 typedef struct {
   int count;
--- compiler_in_c/debug.c
+++ compiler_in_c/debug.c
@@ -162,6 +162,15 @@ int disassembleInstruction(const Chunk *chunk, int offset) {
   case OP_INVOKE: {
     return invokeInstruction("OP_INVOKE", chunk, offset);
   }
+  case OP_INHERIT: {
+    simpleInstruction("OP_INHERIT", offset);
+  }
+  case OP_GET_SUPER: {
+    return constantInstruction("OP_GET_SUPER", chunk, offset);
+  }
+  case OP_SUPER_INVOKE: {
+    return invokeInstruction("OP_SUPER_INVOKE", chunk, offset);
+  }
   default: {
     printf("Unknown opcode %d\n", instruction);
     return offset + 1;
