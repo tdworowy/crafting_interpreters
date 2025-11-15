5b8ec90eda4e4d88c1e6205fd52b6bb613a8f286
Tomasz Dworowy <dworowytomasz@gmail.com>
2025-11-15 10:54:37 +0100

    update

--- compiler_in_rust/compiler/src/scaner.rs
+++ compiler_in_rust/compiler/src/scaner.rs
@@ -1,16 +1,86 @@
-#[derive(Debug)]
-pub struct Scaner {
-    start: char,
-    current: char,
-    line: i32,
+#[derive(Debug, Clone)]
+pub enum TokenType {
+    TOKEN_LEFT_PAREN,
+    TOKEN_RIGHT_PAREN,
+    TOKEN_LEFT_BRACE,
+    TOKEN_RIGHT_BRACE,
+    TOKEN_COMMA,
+    TOKEN_DOT,
+    TOKEN_MINUS,
+    TOKEN_PLUS,
+    TOKEN_SEMICOLON,
+    TOKEN_SLASH,
+    TOKEN_STAR,
+    TOKEN_BANG,
+    TOKEN_BANG_EQUAL,
+    TOKEN_EQUAL,
+    TOKEN_EQUAL_EQUAL,
+    TOKEN_GREATER,
+    TOKEN_GREATER_EQUAL,
+    TOKEN_LESS,
+    TOKEN_LESS_EQUAL,
+    TOKEN_IDENTIFIER,
+    TOKEN_STRING,
+    TOKEN_NUMBER,
+    TOKEN_AND,
+    TOKEN_CLASS,
+    TOKEN_ELSE,
+    TOKEN_FALSE,
+    TOKEN_FOR,
+    TOKEN_FUN,
+    TOKEN_IF,
+    TOKEN_NIL,
+    TOKEN_OR,
+    TOKEN_PRINT,
+    TOKEN_RETURN,
+    TOKEN_SUPER,
+    TOKEN_THIS,
+    TOKEN_TRUE,
+    TOKEN_VAR,
+    TOKEN_WHILE,
+    TOKEN_ERROR,
+    TOKEN_EOF,
 }
 
-impl Scaner {
-    pub fn new(source: char) -> Self {
-        Self {
-            start: source,
-            current: source,
+#[derive(Clone, Debug)]
+pub struct Token<'a> {
+    pub token_type: TokenType,
+    pub lexeme: &'a str,
+    pub line: usize,
+}
+#[derive(Clone, Debug)]
+pub struct Scanner<'a> {
+    source: &'a str,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl<'a> Scanner<'a> {
+    pub fn new(source: &'a str) -> Self {
+        Scanner {
+            source,
+            start: 0,
+            current: 0,
             line: 1,
+        }
+    }
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+    fn make_token(&self, token_type: TokenType) -> Token<'a> {
+        let lexeme = &self.source[self.start..self.current];
+        Token {
+            token_type,
+            lexeme,
+            line: self.line,
+        }
+    }
+    fn error_roken(&self, message: &'a str) -> Token<'a> {
+        Token {
+            token_type: TokenType::TOKEN_ERROR,
+            lexeme: message,
+            line: self.line,
         }
     }
 }
